596c1d1376f56c3809d1b12ee67daae2943d9455
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-08 17:26:29 -0500

    Updated the program class for better timing and readability

--- src/main/scala/org/advent/templates/utils/Problem.scala
+++ src/main/scala/org/advent/templates/utils/Problem.scala
@@ -1,10 +1,21 @@
 package org.advent.templates.utils
 
 import java.time.LocalDate
+import scala.Console.CYAN
+import scala.concurrent.duration.Duration
 import scala.io.Source
 
-case class Result[A](name: String, result: A, elapsedTime: Double, date: LocalDate) {
-  override def toString = s"Advent of Code $date\nOperation: $name\nDuration: $elapsedTime seconds\nResult: $result\n"
+private def Timer[A](codeBlock: => A): (A, Time) = {
+  val startTime = System.nanoTime()
+  (codeBlock, Time(Duration.fromNanos(System.nanoTime() - startTime)))
+}
+
+private class Time(timeElapsed: Duration) {
+  override def toString: String = s"${timeElapsed.toMillis.toString} milliseconds"
+}
+
+private case class Result[A](name: String, result: A, elapsedTime: Time, date: LocalDate) {
+  override def toString = s"${CYAN}Advent of Code $date:\n — Operation: $name\n — Duration: $elapsedTime\n — Result: $result\n"
 }
 
 abstract class Problem[A](year: Int, day: Int) {
@@ -17,21 +28,16 @@ abstract class Problem[A](year: Int, day: Int) {
   def solution2(input: A): Any
 
   def run(): Unit = {
-    val clockStart = System.nanoTime
-    val data = fetchData
-    val fetchLap = System.nanoTime
-    println(Result("Fetch Data", s"${data.size} lines of data", (fetchLap - clockStart) / 1e9d, LocalDate.of(year, 12, day)))
+    val (fetchResult, fetchTime) = Timer { fetchData }
+    println(Result("Fetch Data", s"${fetchResult.size} lines of data", fetchTime, LocalDate.of(year, 12, day)))
 
-    val setupData = setup(data)
-    val setupLap = System.nanoTime
-    println(Result("Setup Data", s"${data.size} lines of setup data", (setupLap - fetchLap) / 1e9d, LocalDate.of(year, 12, day)))
+    val (setupResult, setupTime) = Timer { setup(fetchResult) }
+    println(Result("Setup Data", s"${fetchResult.size} lines of setup data", setupTime, LocalDate.of(year, 12, day)))
 
-    val result1 = solution1(setupData)
-    val result1Lap = System.nanoTime
-    println(Result("Part 1 Solution", result1, (result1Lap - setupLap) / 1e9d, LocalDate.of(year, 12, day)))
+    val (part1Result, part1Time) = Timer { solution1(setupResult) }
+    println(Result("Part 1 Solution", part1Result, part1Time, LocalDate.of(year, 12, day)))
 
-    val result2 = solution2(setupData)
-    val result2Lap = System.nanoTime
-    println(Result("Part 2 Solution", result2, (result2Lap - result1Lap) / 1e9d, LocalDate.of(year, 12, day)))
+    val (part2Result, part2Time) = Timer { solution2(setupResult) }
+    println(Result("Part 2 Solution", part2Result, part2Time, LocalDate.of(year, 12, day)))
   }
 }
