4393f6b602009f334724832f330b9d1f9ae01a47
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-12 15:02:24 -0500

    Updating the rate limiter to actually work

--- src/main/scala/org/advent/utils/HelperClasses.scala
+++ src/main/scala/org/advent/utils/HelperClasses.scala
@@ -5,13 +5,17 @@ import play.api.libs.json._
 import scala.concurrent.duration.*
 
 object HelperClasses {
+  case class CombinedLimiter(part1Answer: Boolean = false, part2Answer: Boolean = false)
 
-  case class PartLimiter(lastRun: Long = -1.seconds.fromNow.time.toNanos,
-                         lastTimeUnit: String = -1.seconds.fromNow.time._2.toString.toLowerCase,
-                         partSolved: Boolean = false)
+  case class DurationDetails(time: Long = -1.seconds.fromNow.time.toNanos,
+                             timeUnit: String = -1.seconds.fromNow.time._2.toString.toLowerCase) {
+    def toFiniteDuration: FiniteDuration = FiniteDuration(time, timeUnit)
+  }
 
-  case class CombinedLimiter(part1: PartLimiter = PartLimiter(), part2: PartLimiter = PartLimiter())
+  case class LimitDetails(lastRun: DurationDetails = DurationDetails(),
+                          dayToPartSoledMap: Map[Int, CombinedLimiter] = Map.empty.withDefaultValue(CombinedLimiter()))
 
-  implicit val partLimiterFormat: Format[PartLimiter] = Json.format[PartLimiter]
   implicit val combinedLimiterFormat: Format[CombinedLimiter] = Json.format[CombinedLimiter]
+  implicit val durationDetailsFormat: Format[DurationDetails] = Json.format[DurationDetails]
+  implicit val limitDetailsFormat: Format[LimitDetails] = Json.format[LimitDetails]
 }
--- src/main/scala/org/advent/utils/RateLimiter.scala
+++ src/main/scala/org/advent/utils/RateLimiter.scala
@@ -2,7 +2,7 @@ package org.advent.utils
 
 import better.files.File
 import cats.effect.IO
-import org.advent.utils.HelperClasses.{CombinedLimiter, PartLimiter}
+import org.advent.utils.HelperClasses.{CombinedLimiter, DurationDetails, LimitDetails}
 import play.api.libs.json.Json
 
 import sys.process.*
@@ -15,38 +15,42 @@ case class RateLimiter(year: Int, day: Int) {
   private val file: File = File(s"src/main/resources/year$year/limiters")
   file.createIfNotExists(asDirectory = false, createParents = true)
 
-  private def pushAnswer(answer: Long, part: Int): IO[PartLimiter] = {
+  private def pushAnswer(answer: Long, part: Int): IO[(DurationDetails, Boolean)] = {
     val result = List("curl", "-X", "POST", "--cookie", s"'session=${sys.env("AOC_COOKIE_SESSION")}'", "-H",
       s"'User-Agent: ${sys.env("AOC_COOKIE_SESSION")}'", "--data", s"'level=$part&answer=$answer'",
       s"https://adventofcode.com/$year/day/$day/answer").mkString(" ") !!
 
+    println(s"Submitting part $part for day $day, Answer for part $part: ${result.contains("That's the right answer") ||
+      result.contains("Did you already complete it")}")
+
     IO {
-      PartLimiter(
-        sys.env("AOC_SUBMISSION_THROTTLE").toInt.minutes.fromNow.time.toNanos,
-        sys.env("AOC_SUBMISSION_THROTTLE").toInt.minutes.fromNow.time._2.toString.toLowerCase,
-        result.contains("That's the right answer") || result.contains("Did you already complete it")
-      )
+      (DurationDetails(sys.env("AOC_SUBMISSION_THROTTLE").toInt.minutes.fromNow.time.toNanos,
+        sys.env("AOC_SUBMISSION_THROTTLE").toInt.minutes.fromNow.time._2.toString.toLowerCase),
+        result.contains("That's the right answer") || result.contains("Did you already complete it"))
     }
   }
 
-  def publishAnswer(part1: Long, part2: Long): IO[Unit] = {
-    if (sys.env("AOC_SUBMIT_ANSWERS").toBoolean) {
-      // Fetch last publish run data for the current part
-      val dayLimiter: Map[Int, CombinedLimiter] =
-        if (file.isEmpty) Map.empty.withDefaultValue(CombinedLimiter())
-        else Json.fromJson[Map[Int, CombinedLimiter]](Json.parse(Source.fromResource(s"year$year/limiters").mkString))
-          .get.withDefaultValue(CombinedLimiter())
+  def publishAnswer(part1: Long, part2: Long): IO[Unit] = if (!sys.env("AOC_SUBMIT_ANSWERS").toBoolean) {
+    // Fetch last publish run data for the current part
+    val dayLimiter: LimitDetails = if (file.isEmpty) LimitDetails() else
+      Json.fromJson[LimitDetails](Json.parse(Source.fromResource(s"year$year/limiters").mkString)).get
 
-      // If duration since last run >= limit && publish enabled submit
-      val ableToRun = List(dayLimiter(day).part1, dayLimiter(day).part2).map {
-        case PartLimiter(_, _, true) => false
-        case PartLimiter(lastRun, lastTimeUnit, _) => Deadline.apply(FiniteDuration.apply(lastRun, lastTimeUnit)).isOverdue()
-      }
+    // Fetch last run time
+    val lastRunTime = Deadline(dayLimiter.lastRun.toFiniteDuration)
+    val dayCombinedLimiter = dayLimiter.dayToPartSoledMap.withDefaultValue(CombinedLimiter())(day)
 
-      // Check results for successful answer / submission and Save new limiter results
-      val newValues = pushAnswer(part1, 1).flatMap(part1Limiter => pushAnswer(part2, 2).map(part2Limiter =>
-        dayLimiter.updated(day, dayLimiter(day).copy(part1 = part1Limiter, part2 = part2Limiter))))
-      newValues.map(values => file.writeText(Json.prettyPrint(Json.toJson(values))))
-    } else IO {}
-  }
+    // Check results for successful answer / submission and Save new limiter results
+    if (lastRunTime.isOverdue()) {
+      val part1Result = if (!dayCombinedLimiter.part1Answer) pushAnswer(part1, 1).map((duration, result) =>
+        (duration, dayCombinedLimiter.copy(part1Answer = result))) else IO { (dayLimiter.lastRun, dayCombinedLimiter) }
+      val part2Result = part1Result.flatMap((duration, combinedLimiter) => if (!dayCombinedLimiter.part2Answer)
+        pushAnswer(part2, 2).map((lastDuration, result) => (lastDuration, combinedLimiter.copy(part2Answer = result))) else
+        IO { (duration, combinedLimiter) })
+      val dayResults = part2Result.map((lastRun, combinedLimiter) =>
+        dayLimiter.copy(lastRun = lastRun, dayToPartSoledMap = dayLimiter.dayToPartSoledMap.updated(day, combinedLimiter)))
+
+      // Update limiter data
+      dayResults.map(limitDetails => file.writeText(Json.prettyPrint(Json.toJson(limitDetails))))
+    } else IO { println(s"Unable to update results, time remaining: ${lastRunTime.timeLeft.toMinutes} minutes") }
+  } else IO {}
 }
