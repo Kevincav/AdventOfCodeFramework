14ce359de522fa57e5445145943966e6d1ebc771
Kevincav <Kevincav@users.noreply.github.com>
2024-12-14 05:08:29 +0000

    Daily input file update

---
+++ src/main/scala/org/advent/year2024/Day14.scala
@@ -0,0 +1,11 @@
+package org.advent.year2024
+
+import org.advent.utils.Problem
+
+object Day14 extends Problem[Boolean](2024, 14) {
+  override def setup(input: List[String]): Boolean = true
+
+  override def solution1(data: Boolean): Long = 0
+
+  override def solution2(data: Boolean): Long = 0
+}
